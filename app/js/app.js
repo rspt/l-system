aacc50b02279baafbccb4b2ada68db7bba012fef
rspt <julien@rspt.ch>
2016-04-05 18:44:11 +0200

    Push for the exhibit

--- app/js/app.js
+++ app/js/app.js
@@ -56,7 +56,6 @@ function draw(path) {
 
   let svg = document.getElementsByTagName('svg')[0]
   let box = svg.getBBox()
-  console.log(box)
   svg.firstChild.setAttribute('transform', `translate(${-box.x}, ${-box.y})`)
   svgContainer.attr('width', box.width).attr('height', box.height)
 }
@@ -81,48 +80,68 @@ function lsystem() {
   draw(constructPath(l.adn))
 }
 
-const inputGeneration = document.querySelector('input#generation')
-inputGeneration.setAttribute('value', generation)
-const inputGenerationStream = Rx.Observable.fromEvent(inputGeneration, 'input')
-inputGenerationStream.subscribe(() => {
-  generation = inputGeneration.value
-  lsystem()
-})
+/////////////////////////
+/////////////////////////
+/////////////////////////
 
-const inputAxiom = document.querySelector('input#axiom')
-inputAxiom.setAttribute('value', axiom)
-const inputAxiomStream = Rx.Observable.fromEvent(inputAxiom, 'input')
-inputAxiomStream.subscribe(() => {
-  axiom = inputAxiom.value
-  lsystem()
-})
-
-const inputAngle = document.querySelector('input#angle')
-inputAngle.setAttribute('value', angle)
-const inputAngleStream = Rx.Observable.fromEvent(inputAngle, 'input')
-inputAngleStream.subscribe(() => {
-  angle = parseInt(inputAngle.value)
-  lsystem()
-})
-
-const inputStep = document.querySelector('input#step')
-inputStep.setAttribute('value', stepLength)
-const inputStepStream = Rx.Observable.fromEvent(inputStep, 'input')
-inputStepStream.subscribe(() => {
-  stepLength = parseInt(inputStep.value)
-  lsystem()
-})
-
-const inputRules = document.querySelector('textarea#rules')
-inputRules.value = rules
-const inputRulesStream = Rx.Observable.fromEvent(inputRules, 'input')
-inputRulesStream.subscribe(() => {
-  rules = []
-  const t = inputRules.value.split(',')
-  while (t[0]) {
-    rules.push(t.splice(0, 2))
+const LSystemText = function() {
+  this.generation = 4
+  this.axiom = 'A'
+  this.rules = 'A,A-B--B+A++AA+B-,B,+A-BB--B-A++A+B'
+  this.angle = 60
+  this.stepLength = 5
+  this.reset = () => {
+    generation = 4
+    axiom = 'A'
+    rules = []
+    const t = 'A,A-B--B+A++AA+B-,B,+A-BB--B-A++A+B'.split(',')
+    while (t[0]) {
+      rules.push(t.splice(0, 2))
+    }
+    angle = 60
+    stepLength = 5
+    lsystem()
   }
-  lsystem()
-})
+}
+
+window.onload = () => {
+  const text = new LSystemText()
+  const gui = new dat.GUI()
+  const guiGeneration = gui.add(text, 'generation', 1, 5).step(1)
+  const guiAxiom = gui.add(text, 'axiom')
+  const guiRules = gui.add(text, 'rules')
+  const guiAngle = gui.add(text, 'angle', 0, 180).step(1)
+  const guiStepLength = gui.add(text, 'stepLength', 1, 100).step(1)
+  const guiReset = gui.add(text, 'reset')
+
+  guiGeneration.onChange((value) => {
+    generation = value
+    lsystem()
+  })
+
+  guiAxiom.onChange((value) => {
+    axiom = value
+    lsystem()
+  })
+
+  guiRules.onChange((value) => {
+    rules = []
+    const t = value.split(',')
+    while (t[0]) {
+      rules.push(t.splice(0, 2))
+    }
+    lsystem()
+  })
+
+  guiAngle.onChange((value) => {
+    angle = value
+    lsystem()
+  })
+
+  guiStepLength.onChange((value) => {
+    stepLength = value
+    lsystem()
+  })
+}
 
 lsystem()
